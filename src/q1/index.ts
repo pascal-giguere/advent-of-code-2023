36009908602932a54556f4e62665885fed92e2e5
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-12-01 17:04:50 -0500

    part 1

--- src/q1/index.ts
+++ src/q1/index.ts
@@ -1,3 +1,10 @@
 export function calculateCalibrationValueSum(input: string) {
-  return 0;
+  const lines: string[] = input.trim().split('\n');
+  return lines.reduce((sum: number, line: string) => sum + extractCalibrationValue(line), 0);
+}
+
+function extractCalibrationValue(line: string): number {
+  const firstDigit: string = Array.from(line).find((char) => char.match(/\d/))!;
+  const lastDigit: string = Array.from(line).reverse().find((char) => char.match(/\d/))!;
+  return parseInt(firstDigit + lastDigit);
 }
--- test/q1/q1.test.ts
+++ test/q1/q1.test.ts
@@ -5,6 +5,14 @@ import {calculateCalibrationValueSum} from "../../src/q1";
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple.txt'), 'utf-8');
 const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'), 'utf-8');
 
-it('basic test', async () => {
-  expect(calculateCalibrationValueSum(SIMPLE_INPUT)).toEqual(77);
+describe('q1', () => {
+  describe('Part 1', () => {
+    it('calculates the sum of calibration values from a simple input', async () => {
+      expect(calculateCalibrationValueSum(SIMPLE_INPUT)).toEqual(142);
+    });
+
+    it('calculates the sum of calibration values from a complex input', async () => {
+      expect(calculateCalibrationValueSum(COMPLEX_INPUT)).toEqual(53194);
+    });
+  });
 });
