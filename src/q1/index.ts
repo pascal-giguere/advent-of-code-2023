ae4cfe1da9f4a5a1157e96d7a641205fe48bec40
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-12-02 09:33:25 -0500

    add part 2

--- src/q1/index.ts
+++ src/q1/index.ts
@@ -1,10 +1,44 @@
-export function calculateCalibrationValueSum(input: string) {
+const numbers: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const NUMBER_REGEX = new RegExp(`(\\d)`, 'g');
+const NUMBER_AS_WORD_REGEX = new RegExp(`(?=(\\d|(?:${Object.keys(numbers).join('|')})))`, 'g');
+
+export function calculateCalibrationValueSum(input: string, config?: { parseNumbersAsWords?: boolean }) {
+  const parseNumbersAsWords: boolean = config?.parseNumbersAsWords ?? false;
   const lines: string[] = input.trim().split('\n');
-  return lines.reduce((sum: number, line: string) => sum + extractCalibrationValue(line), 0);
+  return lines.reduce((sum: number, line: string) => sum + extractCalibrationValue(line, parseNumbersAsWords), 0);
 }
 
-function extractCalibrationValue(line: string): number {
-  const firstDigit: string = Array.from(line).find((char) => char.match(/\d/))!;
-  const lastDigit: string = Array.from(line).reverse().find((char) => char.match(/\d/))!;
-  return parseInt(firstDigit + lastDigit);
+function extractCalibrationValue(line: string, parseNumbersAsWords: boolean): number {
+  const regex: RegExp = parseNumbersAsWords ? NUMBER_AS_WORD_REGEX : NUMBER_REGEX;
+  const matches: string[] = Array.from(line.matchAll(regex)).map((match: RegExpMatchArray) => match[1]);
+  const firstNumber: number = parseNumber(matches[0], parseNumbersAsWords);
+  const lastNumber: number = parseNumber(matches[matches.length - 1], parseNumbersAsWords);
+
+  if (!isFinite(firstNumber) || !isFinite(lastNumber)) {
+    throw new Error(`Could not parse line: ${line}`);
+  }
+
+  return parseInt(`${firstNumber}${lastNumber}`);
+}
+
+function parseNumber(str: string, parseNumbersAsWords: boolean): number {
+  let parsed: number | undefined;
+  if (parseNumbersAsWords) {
+    parsed = numbers[str];
+  }
+  if (parsed === undefined) {
+    parsed = parseInt(str);
+  }
+  return parsed;
 }
--- test/q1/q1.test.ts
+++ test/q1/q1.test.ts
@@ -1,8 +1,9 @@
-import fs from "node:fs";
-import path from "node:path";
-import {calculateCalibrationValueSum} from "../../src/q1";
+import fs from 'node:fs';
+import path from 'node:path';
+import { calculateCalibrationValueSum } from '../../src/q1';
 
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple.txt'), 'utf-8');
+const SIMPLE2_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple-2.txt'), 'utf-8');
 const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'), 'utf-8');
 
 describe('q1', () => {
@@ -13,6 +14,16 @@ describe('q1', () => {
 
     it('calculates the sum of calibration values from a complex input', async () => {
       expect(calculateCalibrationValueSum(COMPLEX_INPUT)).toEqual(53194);
+    });
+  });
+
+  describe('Part 2', () => {
+    it('calculates the sum of calibration values from a simple input - parse numbers as words', async () => {
+      expect(calculateCalibrationValueSum(SIMPLE2_INPUT, { parseNumbersAsWords: true })).toEqual(281);
+    });
+
+    it('calculates the sum of calibration values from a complex input - parse numbers as words', async () => {
+      expect(calculateCalibrationValueSum(COMPLEX_INPUT, { parseNumbersAsWords: true })).toEqual(54249);
     });
   });
 });
