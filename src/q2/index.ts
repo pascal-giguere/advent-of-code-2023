afd9c2bb1f786f0a1b82927448d6f076939d5056
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-12-04 19:57:15 -0500

    add inputs

--- src/q2/index.ts
+++ src/q2/index.ts
@@ -1,1 +1,18 @@
-export function main(input: string) {}
+type Game = {
+  number: number;
+  sets: Set[];
+};
+
+type Set = {
+  redCubes: number;
+  greenCubes: number;
+  blueCubes: number;
+};
+
+export function calculatePossibleGamesSum(input: string): number {}
+
+function parseGame(input: string): Game {}
+
+function isGamePossible(game: Game): boolean {
+  return false;
+}
