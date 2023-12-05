e6f33e4e792e9f2adaf82718702d32da4f4702ff
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-12-04 20:43:09 -0500

    part 1

--- src/q2/index.ts
+++ src/q2/index.ts
@@ -9,10 +9,44 @@ type Set = {
   blueCubes: number;
 };
 
-export function calculatePossibleGamesSum(input: string): number {}
+const MAXIMUM_SET: Set = {
+  redCubes: 12,
+  greenCubes: 13,
+  blueCubes: 14,
+};
 
-function parseGame(input: string): Game {}
+export function calculatePossibleGamesSum(input: string): number {
+  const lines: string[] = input.trim().split('\n');
+  const games: Game[] = lines.map(parseGame);
+  const possibleGames: Game[] = games.filter(isGamePossible);
+  return possibleGames.reduce((sum: number, game: Game) => sum + game.number, 0);
+}
+
+function parseGame(line: string): Game {
+  const match = line.match(/^Game (\d+): (.*)$/);
+  const number = parseInt(match[1]);
+  const sets: Set[] = match[2].split('; ').map(parseSet);
+  return { number, sets };
+}
+
+function parseSet(str: string): Set {
+  const redMatch = str.match(/(\d+) red/);
+  const redCubes = redMatch ? parseInt(redMatch[1]) : 0;
+  const greenMatch = str.match(/(\d+) green/);
+  const greenCubes = greenMatch ? parseInt(greenMatch[1]) : 0;
+  const blueMatch = str.match(/(\d+) blue/);
+  const blueCubes = blueMatch ? parseInt(blueMatch[1]) : 0;
+  return { redCubes, greenCubes, blueCubes };
+}
 
 function isGamePossible(game: Game): boolean {
-  return false;
+  return game.sets.every(isSetPossible);
+}
+
+function isSetPossible(set: Set): boolean {
+  return (
+    set.redCubes <= MAXIMUM_SET.redCubes &&
+    set.greenCubes <= MAXIMUM_SET.greenCubes &&
+    set.blueCubes <= MAXIMUM_SET.blueCubes
+  );
 }
--- test/q2/q2.test.ts
+++ test/q2/q2.test.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { main } from '../../src/q2';
+import { calculatePossibleGamesSum } from '../../src/q2';
 
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple.txt'), 'utf-8');
 const SIMPLE2_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple-2.txt'), 'utf-8');
@@ -8,8 +8,12 @@ const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'),
 
 describe('q2', () => {
   describe('Part 1', () => {
-    it('', async () => {
-      expect(main(SIMPLE_INPUT)).toEqual(142);
+    it('calculates the sum of possible games from a simple input', async () => {
+      expect(calculatePossibleGamesSum(SIMPLE_INPUT)).toEqual(8);
+    });
+
+    it('calculates the sum of possible games from a complex input', async () => {
+      expect(calculatePossibleGamesSum(COMPLEX_INPUT)).toEqual(8);
     });
   });
 
