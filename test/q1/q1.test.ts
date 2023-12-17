6474966d5c369002ca52d802652d8e001e3d9af7
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-12-17 14:28:54 -0500

    esm imports

--- test/q1/q1.test.ts
+++ test/q1/q1.test.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import { calculateCalibrationValueSum } from "../../src/q1";
+import { calculateCalibrationValueSum } from "../../src/q1/index.js";
 
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, "input-simple.txt"), "utf-8");
 const SIMPLE2_INPUT = fs.readFileSync(path.join(__dirname, "input-simple-2.txt"), "utf-8");
@@ -8,21 +8,21 @@ const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, "input-complex.txt"),
 
 describe("q1", () => {
   describe("Part 1", () => {
-    it("calculates the sum of calibration values from a simple input", async () => {
+    it("calculates the sum of calibration values from a simple input", () => {
       expect(calculateCalibrationValueSum(SIMPLE_INPUT)).toEqual(142);
     });
 
-    it("calculates the sum of calibration values from a complex input", async () => {
+    it("calculates the sum of calibration values from a complex input", () => {
       expect(calculateCalibrationValueSum(COMPLEX_INPUT)).toEqual(53194);
     });
   });
 
   describe("Part 2", () => {
-    it("calculates the sum of calibration values from a simple input - parse numbers as words", async () => {
+    it("calculates the sum of calibration values from a simple input - parse numbers as words", () => {
       expect(calculateCalibrationValueSum(SIMPLE2_INPUT, { parseNumbersAsWords: true })).toEqual(281);
     });
 
-    it("calculates the sum of calibration values from a complex input - parse numbers as words", async () => {
+    it("calculates the sum of calibration values from a complex input - parse numbers as words", () => {
       expect(calculateCalibrationValueSum(COMPLEX_INPUT, { parseNumbersAsWords: true })).toEqual(54249);
     });
   });
--- test/q2/q2.test.ts
+++ test/q2/q2.test.ts
@@ -1,27 +1,27 @@
 import fs from "node:fs";
 import path from "node:path";
-import { calculateMinimumSetPowersSum, calculatePossibleGamesSum } from "../../src/q2";
+import { calculateMinimumSetPowersSum, calculatePossibleGamesSum } from "../../src/q2/index.js";
 
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, "input-simple.txt"), "utf-8");
 const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, "input-complex.txt"), "utf-8");
 
 describe("q2", () => {
   describe("Part 1", () => {
-    it("calculates the sum of possible games from a simple input", async () => {
+    it("calculates the sum of possible games from a simple input",  () => {
       expect(calculatePossibleGamesSum(SIMPLE_INPUT)).toEqual(8);
     });
 
-    it("calculates the sum of possible games from a complex input", async () => {
+    it("calculates the sum of possible games from a complex input", () => {
       expect(calculatePossibleGamesSum(COMPLEX_INPUT)).toEqual(2204);
     });
   });
 
   describe("Part 2", () => {
-    it("calculates the sum of powers of minimum sets from a simple input", async () => {
+    it("calculates the sum of powers of minimum sets from a simple input", () => {
       expect(calculateMinimumSetPowersSum(SIMPLE_INPUT)).toEqual(2286);
     });
 
-    it("calculates the sum of powers of minimum sets from a complex input", async () => {
+    it("calculates the sum of powers of minimum sets from a complex input", () => {
       expect(calculateMinimumSetPowersSum(COMPLEX_INPUT)).toEqual(71036);
     });
   });
